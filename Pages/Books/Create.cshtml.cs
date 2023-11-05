14244d40f4dc3ecc3ea88b73360b381aea1a47d8
ANDREEA ARON <andreea.aron1@stud.ubbcluj.ro>
2023-11-05 19:33:59 +0200

    Laboratorul 4 fara sarcina de la final

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -24,6 +24,9 @@ namespace Aron_Andreea1_Lab2.Pages.Books
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
 "PublisherName");
             ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "AuthorName");
+
+            
+
             var book = new Book();
             book.BookCategories = new List<BookCategory>();
             PopulateAssignedCategoryData(_context, book);
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Aron_Andreea1_Lab2.Data;
 using Aron_Andreea1_Lab2.Models;
+using Aron_Andreea1_Lab2.Models.ViewModels;
 
 namespace Aron_Andreea1_Lab2.Pages.Publishers
 {
@@ -19,13 +20,25 @@ namespace Aron_Andreea1_Lab2.Pages.Publishers
             _context = context;
         }
 
-        public IList<Publisher> Publisher { get;set; } = default!;
+        public IList<Publisher> Publisher { get; set; } = default!;
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Publisher != null)
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
